78a69cee6f02fa5cbe7c3c7b39f453741ed4fb14
Anton Demidenko <yaintroversivnost@gmail.com>
2023-09-18 12:57:14 +0500

    Игровое поле и вывод ячеек

---
+++ src/components/Board.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="board">
+    <BoardItem
+      v-for="(cell, index) in cells"
+      :key="cell + '-' + index"
+      :icon="cell"
+    />
+  </div>
+</template>
+
+<script>
+import BoardItem from '@/components/BoardItem';
+
+export default {
+  name: 'Board',
+
+  components: {
+    BoardItem,
+  },
+
+  data() {
+    return {
+      cells: [3, 0, 0, 1, 0, 0, 1, 0, 2, 2, 0, 0, 0, 0, 0, 3],
+    };
+  },
+};
+</script>
+
+<style>
+.board {
+  display: flex;
+  flex-wrap: wrap;
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+}
+</style>
