2c4e343d6c28173f43953f3b6ed41f65fdb3d0e7
Anton Demidenko <yaintroversivnost@gmail.com>
2023-09-18 14:21:24 +0500

    Добавление иконок и улучшение логики

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>Уровень: {{ lvl }}</div>
+
   <div class="board">
     <BoardItem
       v-for="(cell, index) in cells"
@@ -8,8 +10,15 @@
       @mouseup="mouseup(index)"
       @mousemove="go(index)"
       :selected="checkRoad(index)"
-      :closed="checkClosedRoad(index)"
+      :closed="isRoadClosed(index)"
     />
+  </div>
+
+  <div
+    @click="reload()"
+    class="reload"
+  >
+    Сбросить уровень
   </div>
 </template>
 
@@ -25,32 +34,73 @@ export default {
   },
 
   setup() {
-    const cells = ref([3, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3]);
+    let cells = ref([3, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3]);
     const path = ref([]);
     const size = ref(4);
-    const closedPath = ref([])
+    const closedPath = ref([]);
+    const lvl = 1;
 
     const mousedown = (index) => {
       path.value = [];
 
-      if (cells.value[index]) {
+      if (cells.value[index] && !isRoadClosed(index)) {
         path.value.push(index);
       }
     };
 
+    const start = (lvl) => {
+      if (lvl === 1) {
+        cells.value = [3, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3];
+      }
+
+      if (lvl === 2) {
+        cells.value = [3, 1, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 3];
+      }
+
+      size.value = 4;
+      path.value = [];
+      closedPath.value = [];
+    };
+
+    start();
+
+    const reload = () => {
+      start(lvl);
+    };
+
     const mouseup = (index) => {
       if (index !== path.value[0] && cells.value[index] === cells.value[path.value[0]]) {
-        closedPath.value = closedPath.value.concat(path.value)
+        closedPath.value = closedPath.value.concat(path.value);
       }
       path.value = [];
+
+      checkLvl();
+    };
+
+    const checkLvl = () => {
+      let completed = true;
+
+      cells.value.forEach((cell, index) => {
+        if (cell && !isRoadClosed(index)) {
+          completed = false;
+        }
+      });
+
+      if (completed) {
+        alert('Вы выиграли!');
+      }
     };
 
     const go = (index) => {
       if (path.value.length) {
         const lastIndex = path.value[path.value.length - 1];
-        if (Math.abs(lastIndex - index) === 1 || Math.abs(lastIndex - index) === size.value) {
+        if (Math.abs(lastIndex - index) === 1 || (Math.abs(lastIndex - index) === size.value && !isRoadClosed(index))) {
           path.value.push(index);
         }
+      }
+
+      if (isRoadClosed(index) || (cells.value[index] && cells.value[index] !== cells.value[path.value[0]])) {
+        path.value = [];
       }
     };
 
@@ -58,7 +108,7 @@ export default {
       return path.value.findIndex((p) => p === index) > -1;
     };
 
-    const checkClosedRoad = (index) => {
+    const isRoadClosed = (index) => {
       return closedPath.value.findIndex((p) => p === index) > -1;
     };
 
@@ -69,7 +119,9 @@ export default {
       go,
       path,
       checkRoad,
-      checkClosedRoad
+      isRoadClosed,
+      lvl,
+      reload,
     };
   },
 };
@@ -82,6 +134,11 @@ export default {
   flex-wrap: wrap;
   width: 200px;
   height: 200px;
-  margin: 0 auto;
+  margin: 20px auto;
+}
+
+.reload {
+  text-decoration: underline;
+  cursor: pointer;
 }
 </style>
--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="'board-item ' + (selected ? 'selected' : '') + (closed ? 'closed ' : '')">
-    {{ icon ? icon : '' }}
+    <i v-if="icon" :class="'icon icon-' + icon"></i>
   </div>
 </template>
 
@@ -12,24 +12,25 @@ export default {
     icon: {
       type: Number,
       required: false,
-      default: 0
+      default: 0,
     },
     selected: {
       type: Boolean,
       required: false,
-      default: false
+      default: false,
     },
     closed: {
       type: Boolean,
       required: false,
-      default: false
-    }
-  }
-}
+      default: false,
+    },
+  },
+};
 </script>
 
 <style>
 .board-item {
+  position: relative;
   border: 1px solid #2c3e50;
   width: 48px;
   height: 48px;
@@ -41,5 +42,32 @@ export default {
 
 .closed {
   background-color: #00ff0022;
+}
+
+.icon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.icon-1,
+.icon-2,
+.icon-3 {
+  background: url('../assets/icons.png');
+  background-size: 150px 150px;
+}
+
+.icon-1 {
+  background-position: 0 0;
+}
+
+.icon-2 {
+  background-position: 50px 0;
+}
+
+.icon-3 {
+  background-position: 100px 0;
 }
 </style>
